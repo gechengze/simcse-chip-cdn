6e979de6a82aa5ac9f3f39cc2148a107708d6585
gechengze <gechengze@huimei.com>
2024-01-12 18:19:20 +0800

    updata

--- predict.py
+++ predict.py
@@ -37,43 +37,51 @@ def get_embedding(text):
 # with open(f'embedding.pickle', 'wb') as f:
 #     pickle.dump(embedding_dict, f)
 
-embedding_dict = {}
-df = pd.read_csv('data/chip_cdn_test.csv', sep='\t')
-for name in df['原始词']:
-    embedding_dict[name] = get_embedding(name)
-with open(f'embedding_origin.pickle', 'wb') as f:
-    pickle.dump(embedding_dict, f)
+# embedding_dict = {}
+# df = pd.read_csv('data/chip_cdn_test.csv', sep='\t')
+# for name in df['原始词']:
+#     embedding_dict[name] = get_embedding(name)
+# with open(f'embedding_origin.pickle', 'wb') as f:
+#     pickle.dump(embedding_dict, f)
 
-# k = 10
-# df = pd.read_csv('data/code.txt', header=None, names=['code', 'name'], sep='\t')
-# corpus = list(df['name'].values)
-# bm25 = fastbm25(corpus)
-#
-# df_test = pd.read_csv('data/chip_cdn_test.csv', sep='\t')
-# origins, labels, preds = [], [], []
-# for _, row in tqdm(df_test.iterrows(), total=len(df_test)):
-#     if len(row['标准词'].split('##')) > 1:
-#         continue
-#     query = row['原始词']
-#     q_embedding = get_embedding(query)
-#     label = row['标准词']
-#     candidate = bm25.top_k_sentence(query, k=k)
-#     candidate = [x[0] for x in candidate]
-#     # if label not in candidate:
-#     #     candidate = candidate[:-1] + [label]
-#     best_sim = 0
-#     pred = ''
-#     for c in candidate:
-#         c_embedding = get_embedding(c)
-#         sim = F.cosine_similarity(q_embedding, c_embedding, dim=-1).item()
-#         if sim > best_sim:
-#             best_sim = sim
-#             pred = c
-#     origins.append(query)
-#     labels.append(label)
-#     preds.append(pred)
-#
-# df_result = pd.DataFrame({'原始词': origins, '标准词': labels, '预测结果': preds})
-# df_result['是否正确'] = df_result['标准词'] == df_result['预测结果']
-# df_result.to_excel('预测结果.xlsx', index=False)
+
+with open('embedding_origin.pickle', 'rb') as f:
+    origin_embedding = pickle.load(f)
+
+with open('embedding.pickle', 'rb') as f:
+    candidate_embedding = pickle.load(f)
+
+
+k = 10
+df = pd.read_csv('data/code.txt', header=None, names=['code', 'name'], sep='\t')
+corpus = list(df['name'].values)
+bm25 = fastbm25(corpus)
+
+df_test = pd.read_csv('data/chip_cdn_test.csv', sep='\t')
+origins, labels, preds = [], [], []
+for _, row in tqdm(df_test.iterrows(), total=len(df_test)):
+    if len(row['标准词'].split('##')) > 1:
+        continue
+    query = row['原始词']
+    q_embedding = origin_embedding[query]
+    label = row['标准词']
+    candidate = bm25.top_k_sentence(query, k=k)
+    candidate = [x[0] for x in candidate]
+    # if label not in candidate:
+    #     candidate = candidate[:-1] + [label]
+    best_sim = 0
+    pred = ''
+    for c in candidate:
+        c_embedding = candidate_embedding[c]
+        sim = F.cosine_similarity(q_embedding, c_embedding, dim=-1).item()
+        if sim > best_sim:
+            best_sim = sim
+            pred = c
+    origins.append(query)
+    labels.append(label)
+    preds.append(pred)
+
+df_result = pd.DataFrame({'原始词': origins, '标准词': labels, '预测结果': preds})
+df_result['是否正确'] = df_result['标准词'] == df_result['预测结果']
+df_result.to_excel(f'预测结果-top{k}.xlsx', index=False)
 
